5307705ddc69b17117bcbd6764362672f7317a15
Mart4672 <53550558+Mart4672@users.noreply.github.com>
2025-06-22 15:31:34 -0500

    Move remaining RTOS thread functions to EventLoop.cpp; runs correctly on target

--- FreeRTOS/STM32F407VG_FreeRTOS_Blink/src/EventLoop.cpp
+++ FreeRTOS/STM32F407VG_FreeRTOS_Blink/src/EventLoop.cpp
@@ -13,9 +13,58 @@ const osThreadAttr_t neoBlink_attributes = {
 };
 void StartNeoBlink(void *argument);
 
+/* Definitions for blink1 */
+osThreadId_t blink1Handle;
+const osThreadAttr_t blink1_attributes = {
+  .name = "blink1",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+void StartBlink1(void *argument);
+
+/* Definitions for blink2 */
+osThreadId_t blink2Handle;
+const osThreadAttr_t blink2_attributes = {
+  .name = "blink2",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityBelowNormal,
+};
+void StartBlink2(void *argument);
+
+/* Definitions for blink3 */
+osThreadId_t blink3Handle;
+const osThreadAttr_t blink3_attributes = {
+  .name = "blink3",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+void StartBlink3(void *argument);
+
+/* Definitions for blink4 */
+osThreadId_t blink4Handle;
+const osThreadAttr_t blink4_attributes = {
+  .name = "blink4",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityAboveNormal,
+};
+void StartBlink4(void *argument);
+
 // Main Cpp event loop to run application
 void EventLoopCpp()
 {
+    /* Create the thread(s) */
+    /* creation of blink1 */
+    blink1Handle = osThreadNew(StartBlink1, NULL, &blink1_attributes);
+
+    /* creation of blink2 */
+    blink2Handle = osThreadNew(StartBlink2, NULL, &blink2_attributes);
+
+    /* creation of blink3 */
+    blink3Handle = osThreadNew(StartBlink3, NULL, &blink3_attributes);
+
+    /* creation of blink4 */
+    blink4Handle = osThreadNew(StartBlink4, NULL, &blink4_attributes);
+
     /* creation of neoBlink */
     neoBlinkHandle = osThreadNew(StartNeoBlink, NULL, &neoBlink_attributes);
 
@@ -45,6 +94,106 @@ extern "C"
         * time from system startup. 
         */
     }
+}
+
+/* USER CODE BEGIN Header_StartBlink1 */
+/**
+  * @brief  Function implementing the blink1 thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartBlink1 */
+void StartBlink1(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    HAL_GPIO_WritePin(GPIOD, LED1_Pin, GPIO_PIN_SET);
+    osDelay(10);
+	HAL_GPIO_WritePin(GPIOD, LED1_Pin, GPIO_PIN_RESET);
+	osDelay(4990);
+  }
+
+  // In case we accidentally exit from the task loop
+  osThreadTerminate(NULL);
+
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartBlink2 */
+/**
+* @brief Function implementing the blink2 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartBlink2 */
+void StartBlink2(void *argument)
+{
+  /* USER CODE BEGIN StartBlink2 */
+  /* Infinite loop */
+  for(;;)
+  {
+    HAL_GPIO_WritePin(GPIOD, LED2_Pin, GPIO_PIN_SET);
+    osDelay(10);
+    HAL_GPIO_WritePin(GPIOD, LED2_Pin, GPIO_PIN_RESET);
+    osDelay(9990);
+  }
+
+  // In case we accidentally exit from the task loop
+  osThreadTerminate(NULL);
+
+  /* USER CODE END StartBlink2 */
+}
+
+/* USER CODE BEGIN Header_StartBlink3 */
+/**
+* @brief Function implementing the blink3 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartBlink3 */
+void StartBlink3(void *argument)
+{
+  /* USER CODE BEGIN StartBlink3 */
+  /* Infinite loop */
+  for(;;)
+  {
+    HAL_GPIO_WritePin(GPIOD, LED3_Pin, GPIO_PIN_SET);
+    osDelay(10);
+    HAL_GPIO_WritePin(GPIOD, LED3_Pin, GPIO_PIN_RESET);
+    osDelay(49990);
+  }
+
+  // In case we accidentally exit from the task loop
+  osThreadTerminate(NULL);
+
+  /* USER CODE END StartBlink3 */
+}
+
+/* USER CODE BEGIN Header_StartBlink4 */
+/**
+* @brief Function implementing the blink4 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartBlink4 */
+void StartBlink4(void *argument)
+{
+  /* USER CODE BEGIN StartBlink4 */
+  /* Infinite loop */
+  for(;;)
+  {
+    HAL_GPIO_WritePin(GPIOD, LED4_Pin, GPIO_PIN_SET);
+    osDelay(10);
+    HAL_GPIO_WritePin(GPIOD, LED4_Pin, GPIO_PIN_RESET);
+    osDelay(99990);
+  }
+
+  // In case we accidentally exit from the task loop
+  osThreadTerminate(NULL);
+
+  /* USER CODE END StartBlink4 */
 }
 
 /* USER CODE BEGIN Header_StartNeoBlink */
